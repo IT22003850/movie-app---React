0b62792018c9d03c96b8dafc48e38ff275b1e6ba
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-07 23:07:00 +0530

    impliment add to favourite

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Favourites from "./pages/Favourites";
 import NavBar from "./components/NavBar";
+import { MovieProvider } from "./contexts/MovieContext";
 
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
       <main className="main-content">
         <Routes>
@@ -15,7 +16,7 @@ function App() {
           <Route path="/favourites" element={<Favourites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
 }
 
