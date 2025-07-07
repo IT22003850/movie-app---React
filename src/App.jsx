24eb3e45af089bda5456fc1cfa9ca2553515d8fd
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-07 18:24:27 +0530

    favourites page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Favourites from "./pages/Favourites";
 
 function App() {
   return (
-    <>
-    <p>Hello</p>
-    </>
-  )
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourites" element={<Favourites/>} />
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const movies = [
+    { id: 1, title: "tit1", release_date: "2022" },
+    { id: 2, title: "tit1", release_date: "2022" },
+    { id: 3, title: "tit1", release_date: "2022" },
+    { id: 4, title: "tit1", release_date: "2022" },
+    { id: 5, title: "tit1", release_date: "2022" },
+    { id: 6, title: "tit1", release_date: "2022" },
+  ];
+
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    alert(searchQuery)
+    setSearchQuery('')
+  }
+
+  return (
+    <div>
+      <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+          <input
+            type="text"
+            placeholder="Search for movies..."
+            className="search-input"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button type="submit" className="search-button">
+            Search
+          </button>
+        </form>
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
