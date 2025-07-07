cf94ee0d0639a5ffbedb0d13e799bd609c2fde71
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-07 18:34:07 +0530

    NavBar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import "./App.css";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Favourites from "./pages/Favourites";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favourites" element={<Favourites/>} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
